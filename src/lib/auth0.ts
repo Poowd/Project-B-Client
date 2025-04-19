df5f7f7ffbbb5774b3cf8a774187e650579899c5
Poowd <121615238+Poowd@users.noreply.github.com>
2025-04-19 19:06:51 +0800

    tt

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,5 +1,7 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
+// Testing
+
 export const auth0 = new Auth0Client({
   // Options are loaded from environment variables by default
   // Ensure necessary environment variables are properly set
