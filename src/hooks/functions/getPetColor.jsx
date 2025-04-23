5840a0d43b1168574c4fc927ec4a6a01452c8a7c
Poowd <121615238+Poowd@users.noreply.github.com>
2025-04-24 03:30:52 +0800

    Update getPetColor.jsx

--- src/hooks/functions/getPetColor.jsx
+++ src/hooks/functions/getPetColor.jsx
@@ -3,9 +3,5 @@ import { getColor } from "./getColor";
 export const getPetColor = (type, array) => {
   const category = array.filter((category) => category[1] === type);
 
-  if (category) {
-    getColor(category[0][3]);
-  }
-
-  return getColor("dark_gray");
+  return getColor(category[0][3]);
 };
