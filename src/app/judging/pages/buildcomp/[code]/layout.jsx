f887ee98fb4a943b29e25e6bd91e933484c020bd
Mark Lape <121615238+Poowd@users.noreply.github.com>
2025-12-08 22:56:18 +0800

    deleted

--- src/app/judging/pages/buildcomp/[code]/layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <main className="flex justify-center">
-      <section className="text-center lg:text-start px-5 lg:px-0 md:w-4/6">
-        {children}
-      </section>
-    </main>
-  );
-}
--- src/app/tools/[tool]/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useParams } from "next/navigation";
-
-export default function Page() {
-  const params = useParams();
-  return <>The Link is invalid {params.tool}</>;
-}
